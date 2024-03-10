86bb16dc2cf7202d423d1bffeb834a519e15c647
John Ric Merque <johnricmer@gmail.com>
2024-03-10 20:56:38 +0800

    some design changes

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -44,8 +44,8 @@ const HeroSection = () => {
                       src="/images/hero-image.png"
                       alt="hero image"
                       className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
-                      width={300}
-                      height={300}
+                      width={500}
+                      height={500}
                       />
                 </div>
             </div>

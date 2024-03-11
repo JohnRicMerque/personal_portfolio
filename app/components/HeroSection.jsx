d61ac68e67e861cfef3790cb958380342c56ab5a
John Ric Merque <johnricmer@gmail.com>
2024-03-11 21:02:06 +0800

    tried implementing lottie animation

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -2,13 +2,15 @@
 import React from 'react'
 import Image from 'next/image'
 import { TypeAnimation } from 'react-type-animation';
+import Lottie from "lottie-react"
+import animationData from "../assets/dev-animation.json"
 
 const HeroSection = () => {
   return (
     <section className='my-20'> 
         <div className="grid grid-cols-1 sm:grid-cols-12">
             <div className="col-span-7 place-self-center text-center sm:text-left">
-                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold"><span className="text-transparent bg-clip-text bg-gradient-to-br from-green-500 to-blue-200">Hi, I'm{""}</span>
+                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold"><span className="text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-blue-200">Hi, I'm{""}</span>
                 <br />
                 <TypeAnimation
                     sequence={[
@@ -41,12 +43,17 @@ const HeroSection = () => {
             <div className="col-span-5 place-self-center mt-4 lg:mt-0">
                 <div className="w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                     <Image
-                      src="/images/hero-image.png"
+                      src="/images/about-me.png"
                       alt="hero image"
                       className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
-                      width={500}
-                      height={500}
+                      width={300}
+                      height={300}
                       />
+                      {/* <Lottie className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2' 
+                      width={1000}
+                      height={1000} 
+                      animationData={animationData}>
+                      </Lottie> */}
                 </div>
             </div>
         </div>
